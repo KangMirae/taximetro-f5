6e1017121bd273891ee98870c3b920cfe32ecbbd
Mirae Kang <miraemi01@gmail.com>
2025-12-02 13:27:27 +0100

    feat: Print welcome message, #1

---
+++ main.py
@@ -0,0 +1,10 @@
+def taximetro():
+    customer_name = input('Tell us your name: ')
+
+    print("Welcome, {}".format(customer_name))
+
+    print("We're starting our journey. Please, fasten your seatbelt!")
+
+
+if __name__ == "__main__":
+    taximetro()
