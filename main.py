b9feb91fc6c1ef1fd129ebc7b8c281315468331e
Mirae Kang <miraemi01@gmail.com>
2025-12-04 11:47:45 +0100

    feat: Implement time tracking for trip states, #3

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import time
+
+def calc(option, duration):
+    return None
+
 def taximetro():
-    customer_name = input('Tell us your name: ')
 
-    print("Welcome, {}".format(customer_name))
+    # Selected 'Y' to departure, taxi started moving
+    if answer == 'Y':
+        state_start_time = time.time()
+        fare = 0.0
+        
+        while True: 
+            # Here, an event occured. User Chose an option. (input) 
+            duration = time.time() - state_start_time # 이전 옵션부터 지금까지의 초 계산
+            if option == 3: # 도착일 시
+                fare += calc(option, duration)
+                print(f"Total fare is {fare} euros. ")
+                break
+            else: # 이동 또는 멈춤일 시
+                fare += calc(option, duration)
+                state_start_time = time.time()
 
-    print("We're starting our journey. Please, fasten your seatbelt!")
-
-
+    # Ask if user wants to terminate the program
+        
 if __name__ == "__main__":
     taximetro()
